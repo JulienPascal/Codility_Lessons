6516f9c29cbffb943109a16f27ca219010a7d8ff
JulienPascal <julien.pascal@sciencespo.fr>
2019-10-25 07:37:40 +0200

    giving more info

--- 4.Counting_Elements/2.FrogRiverOne/solution.py
+++ 4.Counting_Elements/2.FrogRiverOne/solution.py
@@ -1,12 +1,13 @@
-A = [1, 3, 1, 4, 2, 3, 5, 4]
-
+#A = [1, 3, 1, 4, 2, 3, 5, 4]
+# I am using Python set module. The set l_current stores the leaves already in
+# place at time k. The set l_needed is the minimal set of leaves needed to cross
+# the river. When the two set are equal, it's time to cross the river
 def solution(X, A):
     l_current = set()
     l_needed = set(range(1,X+1))
     time = -1 #initialization
     for k_index, k_value in enumerate(A):
         l_current.add(k_value)
-        #print(l_current)
         if l_current == l_needed:
             time = k_index
             break
--- 6.Sorting/2. Distinct/solution.py
+++ 6.Sorting/2. Distinct/solution.py
@@ -1,4 +1,8 @@
+# For this one, I am Python sets module.
+# The set is updated with the function add()
+# At the end, I simply convert the set to a list and I take its length
 def solution(A):
+    # Set of elements in A, which is updated as we move along A
     l_current = set()
     for k_index, k_value in enumerate(A):
         l_current.add(k_value)
